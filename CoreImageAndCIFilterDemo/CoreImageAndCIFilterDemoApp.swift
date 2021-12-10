a6cec2359673973c738e7fddbb8f8300b7ba534b
Rene Knap <rene.knap.92@googlemail.com>
2021-12-10 12:47:57 +0100

    Initial Commit

---
+++ CoreImageAndCIFilterDemo/CoreImageAndCIFilterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreImageAndCIFilterDemoApp.swift
+//  CoreImageAndCIFilterDemo
+//
+//  Created by Creatcher on 10.12.21.
+//
+
+import SwiftUI
+
+@main
+struct CoreImageAndCIFilterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
